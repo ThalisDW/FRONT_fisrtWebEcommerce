bc40e9064726b7c4be47db7c3e80e7b19e411e6f
Thalis D. W. <ter_twisniewski@arezzo.com.br>
2024-02-16 17:44:42 -0300

    Slice testes

---
+++ src/redux/sliceCarrinho.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+// const carrinho = 
+
+
+// const sliceCarrinho = createSlice({
+//     name: 'sliceCarrinho',
+//     initialState: carrinho,
+//     reducers{}
+// })
